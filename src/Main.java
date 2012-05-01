cbda30b62e38e4be9b66812b138158ebf345fc6f
Rocky Assad <rocky.4q@gmail.com>
2012-04-30 22:19:33 -0500

    removed xml dir. it was being used as a tmp dir

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DestructiveBuilder builder = new DestructiveBuilder("./xml/src2",
-				"./xml/src1");
+		DestructiveBuilder builder = new DestructiveBuilder("./tmp/src2",
+				"./tmp/src1");
 
-		builder.buildDestructiveChanges("./xml");
+		builder.buildDestructiveChanges("./tmp");
 		builder.printDestructiveChanges();
 	}
 }
