9a43a3ce66f0b4eb0c461a111f83ef4eb0216826
Rocky Assad <rocky.4q@gmail.com>
2012-05-01 13:53:43 -0500

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,36 @@
 public class Main {
 
 	/**
-	 * @param args
+	 * @param args[0]
+	 * 
+	 *            The src dir of the salesforce org you're deploying to.
+	 * 
+	 * @param args[1]
+	 * 
+	 *            The src dir of the salesforce org you're deploying from.
+	 * 
+	 * @param args[2]
+	 * 
+	 *            The dir where you want the destructiveChanges.xml file to be
+	 *            created.
 	 */
 	public static void main(String[] args) {
-		DestructiveBuilder builder = new DestructiveBuilder("./tmp/src2",
-				"./tmp/src1");
 
-		builder.buildDestructiveChanges("./tmp");
+		if (args.length != 3) {
+
+			System.out.println("Missing args");
+			System.out
+					.println("Useage: destroy /deploy/to/src/path /deploy/from/src/path /save/destructive/file/to/path");
+			System.exit(1);
+		}
+
+		String pathTo = args[0];
+		String pathFrom = args[1];
+		String saveTo = args[2];
+
+		DestructiveBuilder builder = new DestructiveBuilder(pathFrom, pathTo);
+		builder.buildDestructiveChanges(saveTo);
 		builder.printDestructiveChanges();
+		System.exit(0);
 	}
 }
