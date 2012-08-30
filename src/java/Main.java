9682330ade67357a900c9b6ae0710a5bd7950265
Rocky Assad <rocky.4q@gmail.com>
2012-08-29 19:22:38 -0500

    Now supports dynamic directories. This allows CI servers to inject the pth to the from org for different branches.

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import system.EnvironmentManager;
 import system.Installer;
@@ -15,6 +19,8 @@ public class Main {
 	private final static String optionInstallOnly = "--install-only";
 	private final static String optionPrintOnly = "--print-only";
 	private final static String optionDestroyOnly = "--destroy-only";
+	private final static String optionEnvironment = "-e";
+	private final static String optionPath = "-p";
 
 	/**
 	 * @param args
@@ -60,7 +66,7 @@ public class Main {
 		}
 
 		// Check for valid amount of args
-		if (args.length < 2 || args.length > 3) {
+		if (args.length < 2 || args.length > 4) {
 
 			System.out
 					.println("Useage: java -jar deployAndDestroy.jar [[from env name 'example1'] [to env name 'example2'] [print only '--print-only'] [destroy only '--destroy-only']] [--install-only]");
@@ -68,17 +74,39 @@ public class Main {
 		}
 
 		// Grab environment names
-		String envNameFrom = args[0].toLowerCase();
-		String envNameTo = args[1].toLowerCase();
+		Boolean isEnvPath = (args[0].toLowerCase().equals("-e"));
+		Boolean isArgPath = (args[0].toLowerCase().equals("-p"));
+
+		String envNameFrom = args[1].toLowerCase();
+		String envNameTo = args[2].toLowerCase();
 
 		// Figure out what options were set
 		for (Integer i = 0; i < args.length; i++) {
-			if (i > 1) {
+			if (i > 2) {
 				if (args[i].toLowerCase().equals(optionPrintOnly)) {
 					printOnly = true;
 				} else if (args[i].toLowerCase().equals(optionDestroyOnly)) {
 					destroyOnly = true;
 				}
+			}
+		}
+		
+		if(isArgPath) {
+			// create temp env file for this dir path
+			envNameFrom = "dynamic-env";
+			String dynamicEnvFile = Installer.installPath + File.separator + envNameFrom + ".env";
+			File tmpFile = new File(dynamicEnvFile);
+			
+			Properties prop = new Properties();
+			prop.setProperty("dir.source.root", args[1]);
+			prop.setProperty("type", "directory");
+			
+			try {
+				prop.store(new FileOutputStream(tmpFile), "Dynamic Environment Created by jar option -p");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 
@@ -92,15 +120,17 @@ public class Main {
 		manager.getFromEnvironment().retreive(null);
 		manager.getToEnvironment().retreive(null);
 		long endTime = System.nanoTime();
-		System.out.println("### Retreiving Source Took " + (endTime - startTime) + " ns");
-		
+		System.out.println("### Retreiving Source Took "
+				+ (endTime - startTime) + " ns");
+
 		// Build the destructive changes needed to sync both environments.
 		startTime = System.nanoTime();
 		DestructiveBuilder destroybuilder = new DestructiveBuilder(
 				manager.getFromEnvironment(), manager.getToEnvironment());
 		destroybuilder.buildDestructiveChanges(destroyOnly);
 		endTime = System.nanoTime();
-		System.out.println("### Building Destroyable Changes Took " + (endTime - startTime) + " ns");
+		System.out.println("### Building Destroyable Changes Took "
+				+ (endTime - startTime) + " ns");
 
 		// Tell the deploy builder which environments to work with.
 		DeployBuilder deployBuilder = new DeployBuilder(
@@ -116,7 +146,8 @@ public class Main {
 			startTime = System.nanoTime();
 			deployBuilder.deploy();
 			endTime = System.nanoTime();
-			System.out.println("### Deployment Took " + (endTime - startTime) + " ns");
+			System.out.println("### Deployment Took " + (endTime - startTime)
+					+ " ns");
 		}
 
 		startTime = System.nanoTime();
@@ -126,23 +157,36 @@ public class Main {
 	}
 
 	private static Boolean checkForInvalidArgs(String[] args) {
-	
+
+		Integer count = -1;
 		for (String arg : args) {
+			count++;
 			
-			if(!arg.startsWith("--")) {
+			if (!arg.startsWith("-")) {
 				// Check to make sure env file exists
-				String envFilePath = Installer.installPath + File.separator + arg + ".env";
-				File envFile = new File(envFilePath);
+				String envFilePath = null;
+
+				if (args[0].equals(optionEnvironment) || count == 2) {
+					envFilePath = Installer.installPath + File.separator + arg
+							+ ".env";
+				} else if (args[0].equals(optionPath)) {
+					envFilePath = arg;
+				}
 				
-				if(!envFile.exists()) {
+				File envFile = new File(envFilePath);
+
+				if (!envFile.exists()) {
 					System.out.println("NOPE!");
-					System.out.println("No environment file " + envFilePath + " found.");
+					System.out.println("No environment file " + envFilePath
+							+ " found.");
 					return false;
 				}
 			} else if (!arg.equals(optionInstallOnly)
 					&& !arg.equals(optionDestroyOnly)
-					&& !arg.equals(optionPrintOnly)) {
-				
+					&& !arg.equals(optionPrintOnly)
+					&& !arg.equals(optionEnvironment)
+					&& !arg.equals(optionPath)) {
+
 				System.out.println("NOPE!");
 				System.out.println(arg + " is not a valid option.");
 				return false;
