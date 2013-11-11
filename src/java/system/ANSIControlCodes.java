3d43267e100e880b1753432b97f8aea7f23f5ced
Rocky Assad <r@cky.bz>
2013-11-11 19:13:18 +0000

    Add output coloring

---
+++ src/java/system/ANSIControlCodes.java
@@ -0,0 +1,13 @@
+package system;
+
+public class ANSIControlCodes {
+	
+	public final static String BLACK = "\033[30m";
+	public final static String RED = "\033[31m";
+	public final static String GREEN = "\033[32m";
+	public final static String YELLOW = "\033[33m";
+	public final static String BLUE = "\033[34m";
+	public final static String MAGENTA = "\033[35m";
+	public final static String CYAN = "\033[36m";
+	public final static String WHITE = "\033[37m";
+}
